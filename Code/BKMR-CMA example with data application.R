d104e647825b53482d0b6ceff997266368377efd
Aijin Wang <aijinw@andrew.cmu.edu>
2019-10-03 14:57:46 -0400

    Original Files

---
+++ Code/BKMR-CMA example with data application.R
@@ -0,0 +1,200 @@
+################################################
+###   Bayesian kernel machine regression--   ###
+###   causal mediation analysis code         ###
+###   developed by: Katrina Devick           ###
+###                                          ###
+###   To implement methods presented in      ###
+###   arXiv: 1811.10453                      ###       
+###                                          ###
+###   last updated - 16 September 2019       ###
+################################################
+
+
+## load required library
+library(bkmr)
+
+## load the source file
+## it needs to be in your working directory for it to load as is
+source("source_BKMR_CMA.R")
+
+
+load("../Data/dat_bothclinics.RData")
+
+
+##########################################################
+###                 fit BKMR models                    ###
+##########################################################
+
+
+## let A be a n-by-L matrix containing an exposure mixture comprised of L elements,
+## E.M and E.Y be effect modifiers of exposure-mediator and exposure-outcome relationship respectively,
+## y, a vector of outcome data, and  
+## m, a vector of mediator data
+
+## Now, let Z.M be the exposures and effect modifers E.M and 
+## let Z.Y be the exposures and effect modifers E.Y
+## Z.M <- cbind(A,E.M) 
+## Z.Y <- cbind(A,E.Y) 
+
+## create one more matrix containing the exposures, effect modifier Z.Y and mediator, 
+## precisely in that order (!!! This is very important !!!)
+## Zm.Y <- cbind(Z.Y,m)
+## *** NOTE: the last column of the Zm.Y matrix MUST be your mediator in order for the functions to work properly! ***
+
+
+############################
+##   example from paper   ##
+############################
+dat <- dat.nutr
+
+A <- cbind(dat$as_ln_z,dat$mn_ln_z,dat$pb_ln_z) # exposure
+m <- dat$birthlength_z # mediator
+y <- dat$ccs_z # response
+
+## additional covariates assumed to have a linear effect with the mediator and outcome
+X <- cbind(dat$momage_z,dat$smokenv,dat$sex,dat$momeducd,dat$momIQ_z,dat$homescore_z,dat$proteintert)
+
+E.M <- NULL # effect modifier of exposure-mediator
+E.Y <- dat$age_z # effect modifier of exposure-outcome
+
+Z.M <- cbind(A,E.M)  
+Z.Y <- cbind(A,E.Y) 
+Zm.Y <- cbind(Z.Y,m)
+
+colnames(Z.M)  <- colnames(A)  <- c("As","Mn","Pb")
+colnames(Z.Y)  <- c("As","Mn","Pb","age")
+colnames(Zm.Y) <- c("As","Mn","Pb","age","BL")
+
+
+
+## fit BKMR models for the outcome, TE (outcome without the mediator), and mediator
+## can use any features of BKMR (e.g. varsel=TRUE)
+# set.seed(1)
+# fit.y <- kmbayes(y=y, Z=Zm.Y, X=X, iter=20000, verbose=TRUE, varsel=FALSE) 
+# save(fit.y,file="bkmr_y.RData")
+# 
+# set.seed(2)
+# fit.y.TE <- kmbayes(y=y, Z=Z.Y, X=X, iter=20000, verbose=TRUE, varsel=FALSE) 
+# save(fit.y.TE,file="bkmr_y_TE.RData")
+# 
+# set.seed(3)
+# fit.m <- kmbayes(y=m, Z=Z.M, X=X, iter=20000, verbose=TRUE, varsel=FALSE) 
+# save(fit.m,file="bkmr_m.RData")
+
+
+##### load models 
+
+load("bkmr_y_joint_ageh_nooutliers.RData")
+load("bkmr_y_TE_joint_ageh_nooutliers.RData")
+load("bkmr_m_joint_noage_nooutliers.RData")
+
+fit.y <- fit.y.joint.ageh
+fit.m <- fit.m.joint.noage
+fit.y.TE <- fit.y.TE.joint.ageh
+
+##################################################
+### values at which to predict counterfactuals ###
+##################################################
+
+
+## mean level of confounders
+X.predict <- matrix(colMeans(X),nrow=1)
+
+
+## the change in exposure for which you want to estimate the mediation effects
+
+## We will consider a change in all exposures from their 25th to 75th percentiles 
+## fixing age at testing to its 10th and 90th percentiles 
+## However, this contrast can be anything. 
+
+## if modifiers are considered, you should fix the levels of the modifiers 
+astar       <-   apply(A, 2, quantile, probs=0.25)
+astar.age10 <- c(apply(A, 2, quantile, probs=0.25), quantile(E.Y, probs=0.1))
+astar.age90 <- c(apply(A, 2, quantile, probs=0.25), quantile(E.Y, probs=0.9))
+
+a       <-   apply(A, 2, quantile, probs=0.75)
+a.age10 <- c(apply(A, 2, quantile, probs=0.75), quantile(E.Y, probs=0.1))
+a.age90 <- c(apply(A, 2, quantile, probs=0.75), quantile(E.Y, probs=0.9))
+
+
+## the index of the MCMC iterations to be used for inference 
+sel<-seq(5001,20000,by=15)
+
+
+
+##########################################################
+####             Estimate TE for BKMR                 ####
+##########################################################
+
+## estimate the TE for a change in the exposures from astar to a
+## fixing age at testing to its 10th percentile 
+TE.age10 <- TE.bkmr(a=a.age10, astar=astar.age10, fit.y.TE=fit.y.TE, X.predict=X.predict, alpha = 0.01, sel=sel, seed=122)
+
+## look at the posterior mean, median, and 95% CI for TE
+TE.age10$est
+
+
+# mean      median       lower       upper 
+# -0.40591750 -0.40415149 -0.76625105 -0.08121498 
+
+
+## repeat, now fixing age at testing to its 90th percentile 
+TE.age90 <- TE.bkmr(a=a.age90, astar=astar.age90, fit.y.TE=fit.y.TE, X.predict=X.predict, sel=sel, seed=122)
+
+## look at the posterior mean, median, and 95% CI for TE
+TE.age90$est
+
+
+save(TE.age10, TE.age90, file="../Data/TE_example.RData")
+
+
+##########################################################
+####             Estimate CDE for BKMR                ####
+##########################################################
+
+## estimate the CDE for a change in the exposures from astar to a,
+## fixing the mediator at its 10th, 50th, and 75th percentile and
+## age at testing at its 10th percentile 
+CDE.age10 <- CDE.bkmr(a=a.age10, astar=astar.age10, m.quant=c(0.1,0.5,0.75), fit.y=fit.y, sel=sel, seed=777)
+
+## look at the posterior mean, median, and 95% CI for the CDEs 
+CDE.age10$est
+
+
+CDE.age90 <- CDE.bkmr(a=a.age90, astar=astar.age90, m.quant=c(0.1,0.5,0.75), fit.y=fit.y, sel=sel, seed=777)
+
+## look at the posterior mean, median, and 95% CI for the CDEs 
+CDE.age90$est
+
+save(CDE.age10, CDE.age90, file="../Data/CDE_example.RData")
+
+
+##########################################################
+####             Estimate NDE/NIE for BKMR            ####
+##########################################################
+
+## estimate the TE, NDE and NIE for a change in the exposures from astar to a
+## fixing age at testing to its 90th percentile
+
+## *** NOTE: if the same confounders are used in both the mediation and outcome models, 
+## X.predict.M and X.predict.Y are the same
+
+## *** this step takes a while to run and will take longer for more complex bkmr fits, longer sel vectors and larger K
+mediationeffects.age90 <- mediation.bkmr(a.Y=a.age90, astar.Y=astar.age90, astar.M=astar, fit.m=fit.m, fit.y=fit.y, fit.y.TE=fit.y.TE,
+                                         X.predict.M=X.predict, X.predict.Y=X.predict, sel=sel, seed=22, K=1000)
+## save this object
+save(mediationeffects.age90, file="../Data/mediationeffects_age90.RData")
+
+## look at the posterior mean, median, and 95% CI for the TE, NDE, and NIE
+mediationeffects.age90$est
+
+
+
+mediationeffects.age10 <- mediation.bkmr(a.Y=a.age10, astar.Y=astar.age10, astar.M=astar, fit.m=fit.m, fit.y=fit.y, fit.y.TE=fit.y.TE,
+                                         X.predict.M=X.predict, X.predict.Y=X.predict, sel=sel, seed=22, K=1000)
+## save this object
+save(mediationeffects.age10, file="../Data/mediationeffects_age10.RData")
+
+## look at the posterior mean, median, and 95% CI for the TE, NDE, and NIE
+mediationeffects.age10$est
+
---
+++ Code/source_BKMR_CMA.R
@@ -0,0 +1,149 @@
+##########################################################
+####      Posterior/Bootstrap Summary Function        ####
+##########################################################
+
+
+##### function to calculate mean/median/CI/sd of a vector of posterior/bootstrap samples
+
+postresults <- function(posteriorsamp, alpha){
+  toreturn <- vector()
+  toreturn["mean"]   <- mean(posteriorsamp, na.rm=TRUE)
+  toreturn["sd"]     <- sd(posteriorsamp, na.rm=TRUE)
+  toreturn["lower"]  <- quantile(posteriorsamp, probs=alpha/2, na.rm=TRUE)
+  toreturn["median"]  <- quantile(posteriorsamp, probs=0.5, na.rm=TRUE)
+  toreturn["upper"] <- quantile(posteriorsamp, probs=1-alpha/2, na.rm=TRUE)
+  return(toreturn)
+}
+
+
+##########################################################
+####             Estimate TE for BKMR                 ####
+##########################################################
+
+
+TE.bkmr <- function(a, astar, fit.y.TE, X.predict, alpha=0.05, sel, seed){
+  toreturn <- list()
+  
+  set.seed(seed)
+  newz  <- rbind(a,astar)
+  
+  # give prediction Y for both a and astar
+  TE.mat <- SamplePred(fit.y.TE, Znew = newz, Xnew = X.predict, sel=sel) 
+
+  Ya     <- TE.mat[,"znew1"]
+  Yastar <- TE.mat[,"znew2"]
+  
+  toreturn$TE.samp     <- as.vector(Ya - Yastar)
+  toreturn$Ya.samp     <- as.vector(Ya) 
+  toreturn$Yastar.samp <- as.vector(Yastar) 
+  
+  TE.sum <- postresults(toreturn$TE, alpha=alpha)
+  toreturn$est  <- TE.sum[c("mean","median","lower","upper")]
+  return(toreturn)
+}
+
+
+
+##########################################################
+####           Estimate CDE for BKMR                  ####
+##########################################################
+
+
+CDE.bkmr <- function(a, astar, m.quant, fit.y, X.predict=rep(0,ncol(fit.y$X)), alpha=0.05, sel, seed){
+  toreturn <- list()
+  m <- fit.y$Z[,ncol(fit.y$Z)]  ### okay as long as m is the LAST variable in Zm birthlength
+  Z <- fit.y$Z[,-ncol(fit.y$Z)] # exposure + effect modifier
+  
+  toreturn$est <- matrix(NA, nrow=length(m.quant), ncol=4, dimnames=list(paste0("CDE",m.quant*100), c("mean","median","lower","upper")))
+  for(i in seq_along(m.quant)){
+    print(paste(i, "out of", length(m.quant)))
+    mnew <- quantile(m, probs=m.quant[i]) # mediator set to certain quantile 
+    
+    set.seed(seed)
+    newZm  <- rbind(c(a,mnew),c(astar,mnew)) # exposure(3), 10% age, 10% mediator 
+    CDE.mat <- SamplePred(fit.y, Znew = newZm, Xnew = X.predict, sel=sel) # X.predict is mean of confounders
+    
+    Yam     <- CDE.mat[,"znew1"]
+    Yastarm <- CDE.mat[,"znew2"]
+    
+    CDE <- as.vector(Yam - Yastarm)
+    toreturn[[paste0("CDE",m.quant[i]*100,".samp")]] <- CDE
+    toreturn$est[paste0("CDE",m.quant[i]*100),] <- postresults(CDE, alpha=alpha)[c("mean","median","lower","upper")]
+  }
+  
+  return(toreturn)
+}
+
+
+
+  
+##########################################################
+####           YaMastar counterfactual                ####
+##########################################################
+
+
+YaMastar.SamplePred <- function(a, astar, fit.m, fit.y, X.predict.M, X.predict.Y, sel, seed, K){
+  start.time <- proc.time()
+ 
+  set.seed(seed)
+  EM.samp <- SamplePred(fit.m, Znew = astar, Xnew = X.predict.M, sel=sel) 
+  Mastar     <- as.vector(EM.samp)
+  
+  sigma.samp  <- sqrt(fit.m$sigsq.eps[sel])
+  random.samp <- matrix(rnorm(length(sel)*K),nrow=length(sel),ncol=K)
+  
+  Mastar.samp  <- Mastar + sigma.samp*random.samp
+  
+  YaMastar.samp.mat     <- matrix(NA,nrow=length(sel),ncol=K)
+  for(j in 1:length(sel)){
+    Mastar.j <-  Mastar.samp[j,]
+    aMastar.j <- cbind(matrix(a, nrow=K, ncol=length(a), byrow=TRUE), Mastar.j)
+    row.seed <- j + 10000
+    set.seed(row.seed)
+    YaMastar.j <- SamplePred(fit.y, Znew = aMastar.j, Xnew = X.predict.Y, sel=sel[j])
+    YaMastar.samp.mat[j,] <- as.vector(YaMastar.j)
+    
+    end.time.temp <- proc.time() 
+    if(j%%50==0) print(paste("iter", j, "time: ", round((end.time.temp - start.time)["elapsed"]/60,2),"min")) 
+  }
+  toreturn <- apply(YaMastar.samp.mat,1,mean)
+  
+  return(toreturn)
+}
+
+
+
+##########################################################
+####           Estimate NDE/NIE for BKMR              ####
+##########################################################
+
+
+mediation.bkmr <- function(a.Y, astar.Y, astar.M, m.quant=c(0.25,0.5,0.75), fit.m, fit.y, fit.y.TE, X.predict.M, X.predict.Y, alpha = 0.05, sel, seed, K){
+  
+  toreturn <- list()
+  # there should be a simpler way of running
+  TE <- TE.bkmr(a=a.Y, astar=astar.Y, fit.y.TE=fit.y.TE, X.predict=X.predict.Y, alpha=alpha, sel=sel, seed=(seed+100))
+  
+  Ya     <- TE$Ya.samp
+  Yastar <- TE$Yastar.samp
+  
+  
+  YaMastar <- YaMastar.SamplePred(a=a.Y, astar=astar.M, fit.m=fit.m, fit.y=fit.y,
+                                        X.predict.M=X.predict.M, X.predict.Y=X.predict.Y, sel=sel, seed=seed, K=K)
+  
+  NDE <- YaMastar - Yastar
+  NIE <- Ya - YaMastar
+    
+
+  toreturn$TE.samp <- TE$TE.samp
+  toreturn$NDE.samp <- NDE
+  toreturn$NIE.samp <- NIE
+  
+  toreturn$est <- matrix(NA, nrow=3, ncol=4, dimnames=list(c("TE","NDE","NIE"), c("mean","median","lower","upper")))
+  toreturn$est[c("TE","NDE","NIE"),] <- rbind(postresults(TE$TE.samp, alpha=alpha) [c("mean","median","lower","upper")],
+                                              postresults(NDE, alpha=alpha)[c("mean","median","lower","upper")],
+                                              postresults(NIE, alpha=alpha)[c("mean","median","lower","upper")])
+  
+  return(toreturn)
+}
+
