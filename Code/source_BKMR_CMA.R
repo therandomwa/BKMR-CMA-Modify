c23f06f86768f36a4b818fcad1202b9d4a065832
Aijin Wang <aijinw@andrew.cmu.edu>
2019-11-04 17:30:17 -0500

    Add initial vignette and modify documentation

--- Code/source_BKMR_CMA.R
+++ Code/source_BKMR_CMA.R
@@ -89,6 +89,7 @@ TE.bkmr <- function(a, astar, e.y, fit.y.TE, X.predict.Y, alpha=0.05, sel, seed)
 #' @param a exposure variables at current level
 #' @param astar exposure variables at counterfactual level
 #' @param e.y effect modifier for the outcome variable
+#' @param m.value values that the mediator is set to
 #' @param m.quant values of the quantile that the mediator is set to 
 #' @param fit.y model fit regressing outcome on exposures, effect modifiers, mediator and confounders on outcome
 #' @param alpha 1-confidence interval
@@ -97,12 +98,6 @@ TE.bkmr <- function(a, astar, e.y, fit.y.TE, X.predict.Y, alpha=0.05, sel, seed)
 #' @return Controlled direct effect for BKMR
 CDE.bkmr <- function(a, astar, e.y, m.value=NULL, m.quant=NULL, fit.y, alpha=0.05, sel, seed){
   
-  # if (is.null(m.value) & is.null(m.quant)){
-  #   stop("Must specify either 'm.value' or 'm.quant'")
-  # }
-  # if (!is.null(m.value) & !is.null(m.quant)){
-  #   stop("Must only specify one of 'm.value' and 'm.quant'")
-  # }
   toreturn <- list()
   m <- fit.y$Z[,ncol(fit.y$Z)]  ### okay as long as m is the LAST variable in Zm birthlength
   Z <- fit.y$Z[,-ncol(fit.y$Z)] # exposure + effect modifier
@@ -163,7 +158,8 @@ CDE.bkmr <- function(a, astar, e.y, m.value=NULL, m.quant=NULL, fit.y, alpha=0.0
 #' Get YaMastar used in NDE and NIE caculation
 #' 
 #' @param a exposure variables at current level
-#' @param astar exposure variables at counterfactual level 
+#' @param astar exposure variables at counterfactual level
+#' @param e.y effect modifier for the outcome variable
 #' @param fit.m model fit regressing mediator on exposures and confounders on mediator
 #' @param fit.y model fit regressing outcome on exposures, effect modifiers, mediator and confounders on outcome
 #' @param X.predict.M counfounders for mediator
@@ -217,6 +213,7 @@ YaMastar.SamplePred <- function(a, astar, e.y, fit.m, fit.y, X.predict.M, X.pred
 #' @param X.predict.M counfounders for mediator
 #' @param X.predict.Y counfounders for outcome
 #' @param effects type(s) of effects that users want to output
+#' @param m.value values that the mediator is set to
 #' @param m.quant values of the quantile that the mediator is set to 
 #' @param alpha 1-confidence interval
 #' @param sel a vector selecting which iterations of the fit should be retained or inference
