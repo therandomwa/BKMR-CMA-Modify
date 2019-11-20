aec26a3d4338c6a28a8d75f3acf8d1707393ec3e
Aijin Wang <aijinw@andrew.cmu.edu>
2019-11-20 15:40:30 -0500

    Add CDE risk functions for visualization

--- Code/risk.R
+++ Code/risk.R
@@ -1,30 +1,75 @@
 source("source_BKMR_CMA.R")
 
-TERiskSummaries.CMA <- function(fit.TE, 
-                                    e.y, 
-                                    e.y.name,
-                                    qs = seq(0.25, 0.75, by = 0.05), 
-                                    q.fixed = 0.5, 
-                                    sel) {
+TERiskSummaries.CMA <- function(fit.TE,
+                                e.y=NULL,
+                                e.y.name=NULL,
+                                qs = seq(0.25, 0.75, by = 0.05), 
+                                q.fixed = 0.5, 
+                                alpha = 0.05, 
+                                sel,
+                                seed = 122) {
   
-  toreturn <- data.frame(quantile=qs, est=rep(NA,times=length(qs)), sd=rep(NA,times=length(qs)))
-  
-    for(i in 1:length(qs)){
-      quant <- qs[i]
-      fit <- fit.TE
-      Z <- fit$Z
-      Z = Z[,-which(e.y.name == colnames(Z))]
-      X <- fit$X
-      X.predict <- matrix(colMeans(X),nrow=1)
-      
-      astar <- apply(Z, 2, quantile, q.fixed)
-      a <- apply(Z, 2, quantile, quant)
-      
-      preds = TE.bkmr(a=a, astar=astar, e.y=e.y, fit.y.TE=BKMRfits, X.predict.Y=X.predict, alpha = 0.01, sel=sel, seed=122)
-      toreturn[i, c(2,3)] = c(preds$est["mean"], preds$est["sd"])
-      }
+  toreturn <- data.frame(quantile=qs, 
+                         est=rep(NA,times=length(qs)), 
+                         sd=rep(NA,times=length(qs)))
+  fit <- fit.TE
+  Z <- fit$Z
+  if (!is.null(e.y.name)){
+    Z = Z[,-which(e.y.name == colnames(Z))]
+  }
+  X <- fit$X
+  X.predict <- matrix(colMeans(X),nrow=1)
+  for(i in 1:length(qs)){
+    quant <- qs[i]
+    
+    astar <- apply(Z, 2, quantile, q.fixed)
+    a <- apply(Z, 2, quantile, quant)
+    
+    preds = TE.bkmr(a=a, astar=astar, e.y=e.y, fit.y.TE=fit.TE, 
+                    X.predict.Y=X.predict, alpha = alpha, sel=sel, seed=seed)
+    toreturn[i, c(2,3)] = c(preds$est["mean"], preds$est["sd"])
+  }
   toreturn  
 }
 
-# CDERiskSummaries <- function(fit.y, e.y, e.y.name, 
-#                              qs = seq(0.25, 0.75, by = 0.05), q.fixed = 0.5, sel)
+CDERiskSummaries.CMA <- function(fit.y,
+                                e.y=NULL,
+                                e.y.name=NULL,
+                                m.value = NULL,
+                                m.quant = c(0.1, 0.5, 0.75), 
+                                m.name,
+                                qs = seq(0.25, 0.75, by = 0.05), 
+                                q.fixed = 0.5, 
+                                alpha = 0.05, 
+                                sel,
+                                seed = 122) {
+  if (!is.null(m.value) & !is.null(m.quant)){
+    m.quant = NULL # if both m.value and m.quant are specified, default set to m.value
+  }
+  df <- dplyr::tibble()
+  fit <- fit.y
+  Z <- fit$Z
+  m = Z[,m.name]
+  if (!is.null(e.y.name)){
+    Z = Z[,-which(e.y.name == colnames(Z))]
+  }
+  Z = Z[,-which(m.name == colnames(Z))]
+  X <- fit$X
+  X.predict <- matrix(colMeans(X),nrow=1)
+  for(i in seq_along(qs)) {
+    print(paste("Running", i, "out of", length(qs), "quantile values:"))
+    quant <- qs[i]
+    astar <- apply(Z, 2, quantile, q.fixed)
+    a <- apply(Z, 2, quantile, quant)
+    preds = CDE.bkmr(a, astar, e.y, m.value, m.quant, fit.y, alpha, sel, seed)
+    if (is.null(m.quant)){newm = m.value} else(newm = m.quant)
+    df0 <-dplyr::data_frame(quantile = quant,
+                            m = newm,
+                            est = preds$est[,"mean"],
+                            sd = preds$est[,"sd"])
+    df <- dplyr::bind_rows(df, df0)
+  }
+  df$m = as.factor(df$m)
+  df$quantile = as.factor(df$quantile)
+  df
+}
--- Code/risk_application.R
+++ Code/risk_application.R
@@ -4,9 +4,27 @@ source("risk.R")
 
 sel = seq(5001,20000,by=1000)
 riskSummary10 = TERiskSummaries.CMA(fit.TE = fit.y.TE, e.y=e.y10, e.y.name = "age", sel=sel)
-ggplot(riskSummary10, aes(quantile, est, ymin = est - 1.96*sd, ymax = est + 1.96*sd)) + 
+ggplot(riskSummary10,
+       aes(quantile,
+           est,
+           ymin = est - 1.96 * sd,
+           ymax = est + 1.96 * sd)) +
   geom_pointrange()
 
 riskSummary90 = TERiskSummaries.CMA(fit.TE = fit.y.TE, e.y=e.y90, e.y.name = "age", sel=sel)
-ggplot(riskSummary90, aes(quantile, est, ymin = est - 1.96*sd, ymax = est + 1.96*sd)) + 
+ggplot(riskSummary90,
+       aes(quantile,
+           est,
+           ymin = est - 1.96 * sd,
+           ymax = est + 1.96 * sd)) +
   geom_pointrange()
+
+CDEriskSummary10 = CDERiskSummaries.CMA(fit.y = fit.y, e.y = e.y10, e.y.name = "age", m.name = "BL", sel = sel)
+ggplot(CDEriskSummary10, aes(quantile, est, ymin = est - 1.96*sd, 
+                 ymax = est + 1.96*sd, col = m)) + 
+  geom_pointrange(position = position_dodge(width = 0.75))
+
+CDEriskSummary90 = CDERiskSummaries.CMA(fit.y = fit.y, e.y = e.y90, e.y.name = "age", m.name = "BL", sel = sel)
+ggplot(CDEriskSummary90, aes(quantile, est, ymin = est - 1.96*sd, 
+                             ymax = est + 1.96*sd, col = m)) + 
+  geom_pointrange(position = position_dodge(width = 0.75))
