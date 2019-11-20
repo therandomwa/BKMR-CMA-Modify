b935cf191c8a44886f8dd491bdfdc6fbf5f8da3d
Aijin Wang <aijinw@andrew.cmu.edu>
2019-11-20 18:04:41 -0500

    Add single variable TE risk functions for visualization

--- Code/risk.R
+++ Code/risk.R
@@ -1,48 +1,38 @@
 source("source_BKMR_CMA.R")
 
 TERiskSummaries.CMA <- function(fit.TE,
-                                e.y=NULL,
-                                e.y.name=NULL,
-                                qs = seq(0.25, 0.75, by = 0.05), 
-                                q.fixed = 0.5, 
-                                alpha = 0.05, 
-                                sel,
-                                seed = 122) {
+                                e.y=NULL, e.y.names=NULL,
+                                qs = seq(0.25, 0.75, by = 0.05), q.fixed = 0.5, 
+                                alpha = 0.05, sel, seed = 122) {
   
   toreturn <- data.frame(quantile=qs, 
                          est=rep(NA,times=length(qs)), 
                          sd=rep(NA,times=length(qs)))
   fit <- fit.TE
   Z <- fit$Z
-  if (!is.null(e.y.name)){
-    Z = Z[,-which(e.y.name == colnames(Z))]
+  if (!is.null(e.y.names)){
+    Z = Z[,-which(e.y.names == colnames(Z))]
   }
   X <- fit$X
   X.predict <- matrix(colMeans(X),nrow=1)
   for(i in 1:length(qs)){
     quant <- qs[i]
-    
     astar <- apply(Z, 2, quantile, q.fixed)
     a <- apply(Z, 2, quantile, quant)
-    
     preds = TE.bkmr(a=a, astar=astar, e.y=e.y, fit.y.TE=fit.TE, 
                     X.predict.Y=X.predict, alpha = alpha, sel=sel, seed=seed)
     toreturn[i, c(2,3)] = c(preds$est["mean"], preds$est["sd"])
   }
-  toreturn  
+  return(toreturn)
 }
 
+
+
 CDERiskSummaries.CMA <- function(fit.y,
-                                e.y=NULL,
-                                e.y.name=NULL,
-                                m.value = NULL,
-                                m.quant = c(0.1, 0.5, 0.75), 
-                                m.name,
-                                qs = seq(0.25, 0.75, by = 0.05), 
-                                q.fixed = 0.5, 
-                                alpha = 0.05, 
-                                sel,
-                                seed = 122) {
+                                 e.y=NULL, e.y.names=NULL,
+                                 m.value = NULL, m.quant = c(0.1, 0.5, 0.75), m.name,
+                                 qs = seq(0.25, 0.75, by = 0.05), q.fixed = 0.5, 
+                                 alpha = 0.05, sel, seed = 122) {
   if (!is.null(m.value) & !is.null(m.quant)){
     m.quant = NULL # if both m.value and m.quant are specified, default set to m.value
   }
@@ -50,8 +40,8 @@ CDERiskSummaries.CMA <- function(fit.y,
   fit <- fit.y
   Z <- fit$Z
   m = Z[,m.name]
-  if (!is.null(e.y.name)){
-    Z = Z[,-which(e.y.name == colnames(Z))]
+  if (!is.null(e.y.names)){
+    Z = Z[,-which(e.y.names == colnames(Z))]
   }
   Z = Z[,-which(m.name == colnames(Z))]
   X <- fit$X
@@ -71,5 +61,77 @@ CDERiskSummaries.CMA <- function(fit.y,
   }
   df$m = as.factor(df$m)
   df$quantile = as.factor(df$quantile)
-  df
+  return(df)
+}
+
+
+
+### ***** a combination function of VarRiskSummary and riskSummary.approx for MI BKMR fits ******
+#Compare estimated \code{h} function when a single variable (or a set of variables) is at the 75th versus 25th percentile, when all of the other variables are fixed at a particular percentile
+VarRiskSummary.CMA <-function(whichz = 1,
+                             BKMRfits,
+                             e.y = NULL, e.y.names = NULL, 
+                             qs.diff = c(0.25, 0.75), q.fixed = 0.5,
+                             alpha, sel, seed) {
+  
+  fit <- BKMRfits
+  y <- fit$y
+  Z <- fit$Z
+  X <- fit$X
+  if (!is.null(e.y.names)){
+    Z = Z[,-which(e.y.names == colnames(Z))]
+  }
+  X.predict <- matrix(colMeans(X),nrow=1)
+  
+  a <- astar <- apply(Z, 2, quantile, q.fixed)
+  a[whichz] <- apply(Z[, whichz, drop = FALSE], 2, quantile, qs.diff[1]) # fix z-th variable to the quantile
+  astar[whichz] <- apply(Z[, whichz, drop = FALSE], 2, quantile, qs.diff[2])
+  
+  preds = TE.bkmr(a=a, astar=astar, e.y=e.y, fit.y.TE=fit, 
+                  X.predict.Y=X.predict, alpha = alpha, sel=sel, seed=seed)
+  toreturn = c(preds$est["mean"], preds$est["sd"])
+  names(toreturn) = c("est", "sd")
+  return(toreturn)
+}
+
+
+# Single Variable Risk Summaries
+# 
+# Compute summaries of the risks associated with a change in a single variable in \code{Z} from a single level (quantile) to a second level (quantile), for the other variables in \code{Z} fixed to a specific level (quantile)
+# **** for MI BKMR fits ****
+
+SingVarRiskSummaries.CMA <-function(BKMRfits,
+                                    e.y = NULL, e.y.names = NULL,
+                                    which.z = 1:ncol(BKMRfits$Z),
+                                    z.names = colnames(BKMRfits$Z),
+                                    qs.diff = c(0.25, 0.75),
+                                    q.fixed = c(0.25, 0.50, 0.75),
+                                    alpha = 0.05, sel, seed = 122) {
+  
+  if(is.null(z.names)) z.names <- paste0("z", 1:ncol(BKMRfits$Z))
+  if (!is.null(e.y.names)){
+    Z = BKMRfits$Z
+    Z = Z[,-which(e.y.names == colnames(Z))]
+    which.z = 1:ncol(Z)
+    z.names = colnames(Z)
+  }
+  df <- tibble::tibble()
+  for(i in seq_along(q.fixed)) {
+    for(j in seq_along(which.z)) {
+      risk = VarRiskSummary.CMA(whichz = which.z[j],
+                                BKMRfits = BKMRfits,
+                                e.y = e.y, e.y.names = e.y.names,
+                                qs.diff = qs.diff, q.fixed = q.fixed[i],
+                                alpha = alpha, sel = sel, seed = seed)
+      df0 <- tibble::tibble(q.fixed = q.fixed[i],
+                            variable = z.names[which.z[j]],
+                            est = risk["est"],
+                            sd = risk["sd"])
+      df <- dplyr::bind_rows(df, df0)
+    }
+  }
+  df$variable <- factor(df$variable, levels = z.names[which.z])
+  df$q.fixed = as.factor(df$q.fixed)
+  attr(df, "qs.diff") <- qs.diff
+  return(df)
 }
--- Code/risk_application.R
+++ Code/risk_application.R
@@ -1,8 +1,12 @@
+library(bkmr)
+
 load("../Data/test_data.RData")
 source("source_BKMR_CMA.R")
 source("risk.R")
 
 sel = seq(5001,20000,by=1000)
+
+# overall effects
 riskSummary10 = TERiskSummaries.CMA(fit.TE = fit.y.TE, e.y=e.y10, e.y.name = "age", sel=sel)
 ggplot(riskSummary10,
        aes(quantile,
@@ -10,6 +14,7 @@ ggplot(riskSummary10,
            ymin = est - 1.96 * sd,
            ymax = est + 1.96 * sd)) +
   geom_pointrange()
+
 
 riskSummary90 = TERiskSummaries.CMA(fit.TE = fit.y.TE, e.y=e.y90, e.y.name = "age", sel=sel)
 ggplot(riskSummary90,
@@ -19,12 +24,36 @@ ggplot(riskSummary90,
            ymax = est + 1.96 * sd)) +
   geom_pointrange()
 
+
+
+# CDE 
 CDEriskSummary10 = CDERiskSummaries.CMA(fit.y = fit.y, e.y = e.y10, e.y.name = "age", m.name = "BL", sel = sel)
 ggplot(CDEriskSummary10, aes(quantile, est, ymin = est - 1.96*sd, 
-                 ymax = est + 1.96*sd, col = m)) + 
+                             ymax = est + 1.96*sd, col = m)) + 
   geom_pointrange(position = position_dodge(width = 0.75))
+
 
 CDEriskSummary90 = CDERiskSummaries.CMA(fit.y = fit.y, e.y = e.y90, e.y.name = "age", m.name = "BL", sel = sel)
 ggplot(CDEriskSummary90, aes(quantile, est, ymin = est - 1.96*sd, 
                              ymax = est + 1.96*sd, col = m)) + 
   geom_pointrange(position = position_dodge(width = 0.75))
+
+
+
+# single variable total effects
+risks.singvar10 = SingVarRiskSummaries.CMA(BKMRfits = fit.y.TE,
+                                           e.y=e.y10, e.y.names="age",
+                                           sel=sel)
+ggplot(risks.singvar10, aes(variable, est, ymin = est - 1.96*sd, 
+                            ymax = est + 1.96*sd, col = q.fixed)) + 
+  geom_pointrange(position = position_dodge(width = 0.75)) + 
+  coord_flip()
+
+
+risks.singvar90 = SingVarRiskSummaries.CMA(BKMRfits = fit.y.TE,
+                                           e.y=e.y90, e.y.names="age",
+                                           sel=sel)
+ggplot(risks.singvar90, aes(variable, est, ymin = est - 1.96*sd, 
+                            ymax = est + 1.96*sd, col = q.fixed)) + 
+  geom_pointrange(position = position_dodge(width = 0.75)) + 
+  coord_flip()
